436255925469cc138187a744567f67fa534e53bb
Hong, Chang Nam <hcn1519@gmail.com>
2016-11-11 20:16:21 +0900

    데이터 랜더링 로직 완료

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,16 +10,42 @@ class HomeController < ApplicationController
 
   end
 
+  require 'time'
+
   def result
     @building = paramCheck(params[:building])
+    @selectedBuilding = ClassRoom.where(:building_name => @building)
+    @resultTable = Hash.new{|hsh,key| hsh[key] = [] }
+
+    @today = Date.today.strftime("%A").downcase
+    @today = @today[0, 3] # 요일의 앞의 3글자만 받아오기
+
+    currentTime = Time.now.change(hour: 10)
+    @selectedBuilding.each_with_index do |b, i|
+      time = ClassTime.where(:class_room_id => b.id)
+      time.each do |t|
+        if @today == t.dayoftheWeek # 요일 매칭
+          # 현재 시간 4시 35분
+          # 현재 시간의 구간에 걸쳐 있는 강의실 출력 + 이후 강의
+          # 04:30 ~ 05:45
+          if currentTime <= Time.parse(t.to)
+            @resultTable[b.full_name] << [t.from, t.to]
+          end
+        end
+      end
+      unless @resultTable.nil?
+        @resultTable[b.full_name].sort!
+      end
+    end
+    @reverseTable = reverseTime(@resultTable, currentTime)
   end
 
   private
   def paramCheck(para)
     if para == "0"
-      building = "K"
+       building = "K"
     elsif para == "1"
-      building = "J"
+       building = "J"
     elsif para == "2"
       building = "MA"
     elsif para == "3"
@@ -35,7 +61,31 @@ class HomeController < ApplicationController
     elsif para == "8"
       building = "GA"
     end
-
     building
   end
+
+
+  def reverseTime(resultTable, currentTime)
+    reverseTable = Hash.new{|hsh,key| hsh[key] = [] }
+
+    resultTable.each do |key, value|
+      time = ["09:00", "10:30", "12:00", "13:30", "15:00", "16:30"]
+      value.each do |v|
+        if time.include?(v[0])
+          time.delete(v[0])
+        end
+      end
+      time.each do |t|
+        if currentTime > Time.parse(t)
+          time.delete(t)
+        end
+      end
+
+      reverseTable[key] << time
+    end
+
+    reverseTable
+  end
+
+
 end
